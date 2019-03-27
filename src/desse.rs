9c211d4872e55f325f487c9a88aabf06fd7b175e
Devashish Dixit <devashish@crypto.com>
2019-03-27 14:13:56 +0800

    Implemented Desse trait for unsigned integers

--- src/desse.rs
+++ src/desse.rs
@@ -1,11 +1,72 @@
-/// Any type must implement this trait for serialization and deserialization
-pub trait Desse: Sized {
-    /// Returns the size of byte array returned on serialization
-    fn serialize_size(&self) -> u64;
+use byteorder::{ByteOrder, LittleEndian};
 
+/// Any type must implement this trait for serialization and deserialization
+pub trait Desse<T>: Sized {
     /// Serializes current object
-    fn serialize(&self) -> Vec<u8>;
+    fn serialize(&self) -> T;
 
     /// Deserializes an object from byte
-    fn deserialize_from(bytes: &[u8]) -> Self;
+    fn deserialize_from(bytes: &T) -> Self;
+}
+
+impl Desse<[u8; 1]> for u8 {
+    fn serialize(&self) -> [u8; 1] {
+        [*self]
+    }
+
+    fn deserialize_from(bytes: &[u8; 1]) -> Self {
+        bytes[0]
+    }
+}
+
+impl Desse<[u8; 2]> for u16 {
+    fn serialize(&self) -> [u8; 2] {
+        let mut bytes: [u8; 2] = Default::default();
+        LittleEndian::write_u16(&mut bytes, *self);
+
+        bytes
+    }
+
+    fn deserialize_from(bytes: &[u8; 2]) -> Self {
+        LittleEndian::read_u16(bytes)
+    }
+}
+
+impl Desse<[u8; 4]> for u32 {
+    fn serialize(&self) -> [u8; 4] {
+        let mut bytes: [u8; 4] = Default::default();
+        LittleEndian::write_u32(&mut bytes, *self);
+
+        bytes
+    }
+
+    fn deserialize_from(bytes: &[u8; 4]) -> Self {
+        LittleEndian::read_u32(bytes)
+    }
+}
+
+impl Desse<[u8; 8]> for u64 {
+    fn serialize(&self) -> [u8; 8] {
+        let mut bytes: [u8; 8] = Default::default();
+        LittleEndian::write_u64(&mut bytes, *self);
+
+        bytes
+    }
+
+    fn deserialize_from(bytes: &[u8; 8]) -> Self {
+        LittleEndian::read_u64(bytes)
+    }
+}
+
+impl Desse<[u8; 16]> for u128 {
+    fn serialize(&self) -> [u8; 16] {
+        let mut bytes: [u8; 16] = Default::default();
+        LittleEndian::write_u128(&mut bytes, *self);
+
+        bytes
+    }
+
+    fn deserialize_from(bytes: &[u8; 16]) -> Self {
+        LittleEndian::read_u128(bytes)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![deny(missing_docs)]
-//! This crate exposes functionality for serialization and deserialization of types with a constant size, known at 
-//! compile time. Any dynamically allocated types, such as, [`String`](std::string::String), [`Vec`](std::vec::Vec), 
+//! This crate exposes functionality for serialization and deserialization of types with a constant size, known at
+//! compile time. Any dynamically allocated types, such as, [`String`](std::string::String), [`Vec`](std::vec::Vec),
 //! [`HashMap`](std::collections::HashMap), etc. cannot be serialized using this crate.
-//! 
+//!
 //! ## Future improvements
 //! Once [`const_generics`](https://github.com/rust-lang/rfcs/blob/master/text/2000-const-generics.md) is implemented
 //! in Rust, we can start adding variable size statically allocated types in this library.
