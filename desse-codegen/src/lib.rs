106f65e509bb97c2a4cf19001146a8c1ef22c112
Devashish Dixit <devashish@crypto.com>
2019-03-28 16:59:00 +0800

    Sample syn, quote macro

--- desse-codegen/src/lib.rs
+++ desse-codegen/src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+extern crate syn;
+
+use syn::Item;
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn desse(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item: Item = syn::parse(item).expect("Failed to parse input");
+    
+    unimplemented!()
 }
