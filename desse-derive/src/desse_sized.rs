b73546b8c68ec68befdef3cba6f85482805dd1a2
Devashish Dixit <devashish@crypto.com>
2019-04-01 12:31:06 +0800

    Minor change in desse_sized derive macro implementation and added initial structure of desse derive macro implementation

--- desse-derive/src/desse_sized.rs
+++ desse-derive/src/desse_sized.rs
@@ -7,13 +7,11 @@ use syn::{DataEnum, DataStruct, DataUnion, DeriveInput, Type};
 pub fn get_desse_sized_impl(input: DeriveInput) -> TokenStream {
     let name = input.ident;
 
-    let types: Vec<&Type> = match &input.data {
-        Struct(ref struct_data) => get_struct_types(struct_data),
-        Enum(ref enum_data) => get_enum_types(enum_data),
-        Union(ref union_data) => get_union_types(union_data),
+    let expr = match &input.data {
+        Struct(ref struct_data) => get_struct_expr(struct_data),
+        Enum(ref enum_data) => get_enum_expr(enum_data),
+        Union(ref union_data) => get_union_expr(union_data),
     };
-
-    let expr = get_size_expr(types);
 
     let output = quote! {
         #[automatically_derived]
@@ -26,12 +24,17 @@ pub fn get_desse_sized_impl(input: DeriveInput) -> TokenStream {
     output
 }
 
-fn get_enum_types(_enum_data: &DataEnum) -> Vec<&Type> {
+fn get_enum_expr(_enum_data: &DataEnum) -> TokenStream {
     panic!("This macro cannot be used on enums!")
 }
 
-fn get_union_types(_union_data: &DataUnion) -> Vec<&Type> {
+fn get_union_expr(_union_data: &DataUnion) -> TokenStream {
     panic!("This macro cannot be used on unions!")
+}
+
+fn get_struct_expr(struct_data: &DataStruct) -> TokenStream {
+    let types = get_struct_types(struct_data);
+    get_size_expr(types)
 }
 
 fn get_struct_types(struct_data: &DataStruct) -> Vec<&Type> {
