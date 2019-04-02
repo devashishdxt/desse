ced02e90ca78f4fa84e05d2d623f1d31f23e4b65
Devashish Dixit <devashish@crypto.com>
2019-04-02 13:54:01 +0800

    Added usage section in documentation

--- README.md
+++ README.md
@@ -18,6 +18,40 @@ struct MyStruct {
 
 `Desse::serialize` will serialize this struct in `[u8; 3]` where `3` is the sum of sizes of `u8` and `u16`.
 
+## Usage
+Add `desse` in your `Cargo.toml`'s `dependencies` section.
+```
+[dependencies]
+desse = "0.1"
+```
+
+`Desse` trait can be implemented for any struct (whose size is known at compile time) using `derive` macro. This
+crate also provides a `derive` macro for implementing `DesseSized` trait which is necessary for implementing `Desse`
+trait.
+```
+use desse::{Desse, DesseSized};
+
+#[derive(Desse, DesseSized)]
+struct MyStruct {
+    a: u8,
+    b: u16,
+}
+```
+
+Now, you can use `Desse::serialize` and `Desse::deserialize_from` for serialization and deserialization of this 
+struct.
+
+```
+let my_struct = MyStruct { a: 5, b: 1005 };
+let serialized: [u8; 3] = my_struct.serialize();
+let new_struct = MyStruct::deserialize_from(&serialized);
+
+assert_eq!(my_struct, new_struct);
+```
+
+Note that `Desse::serialize` returns an array of fixed length (`3` in above case) and `Desse::deserialize` takes
+reference to an array of fixed length as argument.
+
 ## Performance
 This crate values performance more than anything. We don't shy away from using tested and verified **unsafe** code
 if it improves performance.
--- desse/src/lib.rs
+++ desse/src/lib.rs
@@ -1,8 +1,8 @@
 #![deny(missing_docs)]
 //! Ultra fast binary serialization and deserialization for types with a constant size (known at compile time). This
 //! crate cannot be used to serialize or deserialize dynamically allocated types, such as,
-//! [`String`](std::string::String), [`Vec`](std::vec::Vec), [`HashMap`](std::collections::HashMap), etc., and types 
-//! with unknown size at compile time such as `slices`, `&str`, etc.
+//! [`String`](std::string::String), [`Vec`](std::vec::Vec), [`HashMap`](std::collections::HashMap), etc., and types
+//! with unknown size at compile time such as [`slice`](std::slice), [`&str`](std::str), etc.
 //!
 //! ## Binary Encoding Scheme
 //!
@@ -18,6 +18,40 @@
 //! ```
 //!
 //! `Desse::serialize` will serialize this struct in `[u8; 3]` where `3` is the sum of sizes of `u8` and `u16`.
+//!
+//! ## Usage
+//! Add `desse` in your `Cargo.toml`'s `dependencies` section.
+//! ```
+//! [dependencies]
+//! desse = "0.1"
+//! ```
+//!
+//! `Desse` trait can be implemented for any struct (whose size is known at compile time) using `derive` macro. This
+//! crate also provides a `derive` macro for implementing `DesseSized` trait which is necessary for implementing `Desse`
+//! trait.
+//! ```
+//! use desse::{Desse, DesseSized};
+//!
+//! #[derive(Desse, DesseSized)]
+//! struct MyStruct {
+//!     a: u8,
+//!     b: u16,
+//! }
+//! ```
+//!
+//! Now, you can use `Desse::serialize` and `Desse::deserialize_from` for serialization and deserialization of this
+//! struct.
+//!
+//! ```
+//! let my_struct = MyStruct { a: 5, b: 1005 };
+//! let serialized: [u8; 3] = my_struct.serialize();
+//! let new_struct = MyStruct::deserialize_from(&serialized);
+//!
+//! assert_eq!(my_struct, new_struct);
+//! ```
+//!
+//! Note that `Desse::serialize` returns an array of fixed length (`3` in above case) and `Desse::deserialize` takes
+//! reference to an array of fixed length as argument.
 //!
 //! ## Performance
 //!
