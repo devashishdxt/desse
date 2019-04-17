9d9026e67b041a609a89a9f24a8568506152b065
Devashish Dixit <devashishdxt@gmail.com>
2019-04-18 02:58:52 +0800

    Rough version of enum support with non-unit variants

--- desse-derive/src/expr/deserialize_expr.rs
+++ desse-derive/src/expr/deserialize_expr.rs
@@ -1,7 +1,6 @@
 use proc_macro2::TokenStream;
-use quote::quote;
-use syn::punctuated::Punctuated;
-use syn::{DataEnum, DataStruct, Field, Fields, Ident};
+use quote::{quote, ToTokens};
+use syn::{DataEnum, DataStruct, Fields, Ident, Index};
 
 use crate::expr::SizeExpr;
 
@@ -9,79 +8,97 @@ use crate::expr::SizeExpr;
 pub struct DeserializeExpr;
 
 impl DeserializeExpr {
-    /// Calculates deserialize expression for punctuated fields
-    fn get_deserialize_expr_for_punctuated_field<T>(
-        punctuated: &Punctuated<Field, T>,
-    ) -> TokenStream {
-        if punctuated.is_empty() {
-            panic!("Cannot implement serialize for struct with no fields");
-        }
-
-        let mut exprs = Vec::with_capacity(punctuated.len());
-        let mut counter = quote! { 0 };
-
-        for (i, field) in punctuated.iter().enumerate() {
-            let name = match &field.ident {
-                None => quote! { #i },
-                Some(ref ident) => quote! { #ident },
-            };
-
-            let ty = &field.ty;
-
-            exprs.push(
-            quote! { object. #name = <#ty>::deserialize_from(&*(bytes[ (#counter) .. ( #counter + <#ty>::SIZE ) ].as_ptr() as *const [u8; <#ty>::SIZE])); },
-        );
-
-            counter = quote! { #counter + <#ty>::SIZE };
-        }
-
-        quote! {
-            let mut object: Self = unsafe { std::mem::zeroed() };
-            unsafe { #(#exprs)* }
-            object
-        }
-    }
-
     /// Calculates serialize expression for fields
-    fn get_deserialize_expr_for_fields(fields: &Fields) -> TokenStream {
+    fn get_deserialize_expr_for_fields<T: ToTokens, C: ToTokens>(
+        container_name: T,
+        init_counter: C,
+        fields: &Fields,
+    ) -> TokenStream {
         match fields {
-            Fields::Unit => quote! { 0 },
+            Fields::Unit => quote! { #container_name },
             Fields::Named(named_fields) => {
-                Self::get_deserialize_expr_for_punctuated_field(&named_fields.named)
+                if named_fields.named.is_empty() {
+                    quote! { #container_name {} }
+                } else {
+                    let mut exprs = Vec::with_capacity(named_fields.named.len());
+                    let mut counter = quote! { #init_counter };
+
+                    for field in named_fields.named.iter() {
+                        let field_name = match &field.ident {
+                            None => unreachable!(),
+                            Some(ref ident) => quote! { #ident },
+                        };
+                        let field_type = &field.ty;
+
+                        exprs.push(quote! {
+                            #field_name: <#field_type>::deserialize_from(&*(bytes[ (#counter) .. ( #counter + <#field_type>::SIZE ) ].as_ptr() as *const [u8; <#field_type>::SIZE]))
+                        });
+
+                        counter = quote! { #counter + <#field_type>::SIZE };
+                    }
+
+                    quote! {
+                        unsafe {
+                            #container_name {
+                                #(#exprs),*
+                            }
+                        }
+                    }
+                }
             }
             Fields::Unnamed(unnamed_fields) => {
-                Self::get_deserialize_expr_for_punctuated_field(&unnamed_fields.unnamed)
+                if unnamed_fields.unnamed.is_empty() {
+                    quote! { #container_name() }
+                } else {
+                    let mut exprs = Vec::with_capacity(unnamed_fields.unnamed.len());
+                    let mut counter = quote! { #init_counter };
+
+                    for field in unnamed_fields.unnamed.iter() {
+                        let field_type = &field.ty;
+
+                        exprs.push(quote! {
+                            <#field_type>::deserialize_from(&*(bytes[ (#counter) .. ( #counter + <#field_type>::SIZE ) ].as_ptr() as *const [u8; <#field_type>::SIZE]))
+                        });
+
+                        counter = quote! { #counter + <#field_type>::SIZE };
+                    }
+
+                    quote! {
+                        unsafe {
+                            #container_name(#(#exprs),*)
+                        }
+                    }
+                }
             }
         }
     }
 
     /// Calculates size expression for [`DataStruct`](syn::DataStruct)
-    pub fn for_struct(struct_data: &DataStruct) -> TokenStream {
-        Self::get_deserialize_expr_for_fields(&struct_data.fields)
+    pub fn for_struct(name: &Ident, struct_data: &DataStruct) -> TokenStream {
+        Self::get_deserialize_expr_for_fields(name, quote! { 0 }, &struct_data.fields)
     }
 
     /// Calculates size expression for [`DataEnum`](syn::DataEnum)
-    ///
-    /// # Todo
-    ///
-    /// Currently, this function takes `name` as input for referencing different variants of enum, for example,
-    /// `MyEnum::Variant1`. The need for this will go away in future once
-    /// [`type_alias_enum_variants`](https://github.com/rust-lang/rust/issues/49683) lands in stable which will enable
-    /// us to write `Self::Variant1` instead.
     pub fn for_enum(name: &Ident, enum_data: &DataEnum) -> TokenStream {
         let variant_count = enum_data.variants.len();
 
         let size_type = SizeExpr::get_variant_count_size_type(variant_count);
         let mut match_exprs = Vec::with_capacity(variant_count);
 
-        let variant = quote! {
-            let variant = <#size_type>::deserialize_from(bytes) as usize;
+        let variant_expr = quote! {
+            let variant = unsafe { <#size_type>::deserialize_from(&*(bytes[0..<#size_type>::SIZE].as_ptr() as *const [u8; <#size_type>::SIZE])) };
         };
 
-        for (i, field) in enum_data.variants.iter().enumerate() {
-            let variant_name = &field.ident;
+        for (i, variant) in enum_data.variants.iter().enumerate() {
+            let variant_name = &variant.ident;
+            let variant_expr = Self::get_deserialize_expr_for_fields(
+                quote! { #name:: #variant_name},
+                quote! { <#size_type>::SIZE },
+                &variant.fields,
+            );
+            let index = Index::from(i);
             match_exprs.push(quote! {
-                #i => #name:: #variant_name
+                #index => #variant_expr
             });
         }
 
@@ -96,7 +113,7 @@ impl DeserializeExpr {
         };
 
         quote! {
-            #variant
+            #variant_expr
             #match_expr
         }
     }
